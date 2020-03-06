895b0983cd327c2cce072e581994cd7ddd4f9070
Fernando Hidemi Uchiyama <fernandoservico@gmail.com>
2020-03-06 11:24:43 -0300

    Create app.module.ts

---
+++ app/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { BoardComponent } from './components/board/board.component';
+import { SquareComponent } from './components/square/square.component';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameComponent,
+    BoardComponent,
+    SquareComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
